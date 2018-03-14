d5598f1cb0d7c06bd682c3fee109b7b66cde8be7
madgrid <aaronmadved@gmail.com>
2018-03-14 00:04:12 -0400

    Function to list currencies

--- src/app/pages/donation/donation.component.ts
+++ src/app/pages/donation/donation.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from "@angular/router";
+
 
 @Component({
-  selector: 'app-donation',
-  templateUrl: './donation.component.html',
-  styleUrls: ['./donation.component.scss']
+    selector: 'app-donation',
+    templateUrl: './donation.component.html',
+    styleUrls: ['./donation.component.scss']
 })
 export class DonationComponent implements OnInit {
+    cryptos: Object[];
 
-  constructor() { }
+    constructor(private router: Router) { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        this.cryptos = ['btc', 'eth', 'etc', 'etp'];
+    }
 
+    gotoCurrency(currency:string) {
+        console.log(currency)
+        this.router.navigate(['donation', currency]);
+    }
 }
