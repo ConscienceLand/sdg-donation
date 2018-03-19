8e6967c386714e7ee240cab973ed1d102d4f2705
madgrid <aaronmadved@gmail.com>
2018-03-19 00:11:24 -0400

    Set cookie on login

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -3,6 +3,7 @@ import { Router } from "@angular/router";
 import { DonationService } from '../../service/donation.service'
 import { NotificationsService } from 'angular2-notifications';
 import { AlertService } from '../../service/alert.service';
+import { CookieService } from 'ngx-cookie-service';
 
 @Component({
     selector: 'app-login',
@@ -17,6 +18,7 @@ export class LoginComponent implements OnInit {
                 private router: Router,
                 private notificationsService: NotificationsService,
                 private alert: AlertService,
+                private cookieService: CookieService
                	) {
         this.input = {"code": ""};
     }
@@ -27,7 +29,10 @@ export class LoginComponent implements OnInit {
     public login() {
         if(this.input.code) {
             this.donation.login(this.input.code)
-                .then(()=>this.router.navigate(["/account"]))
+                .then(()=>{
+                    this.cookieService.set('code', this.input.code)
+                    this.router.navigate(["/account"])
+                })
                 .catch((error)=>{
                     console.log(error)
                     this.alert.errorToast('Error', 'Wrong Passcode');
