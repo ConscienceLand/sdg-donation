35b7c4d7ca9a155611453c553e104958e2155833
madgrid <aaronmadved@gmail.com>
2018-03-19 00:12:50 -0400

    Added new api's

--- src/app/service/donation.service.ts
+++ src/app/service/donation.service.ts
@@ -4,7 +4,7 @@ import { HttpClient, HttpParams } from '@angular/common/http';
 @Injectable()
 export class DonationService {
 
-    private SERVER='https://donations.caringcurrency.com/';
+    private SERVER='https://donations.caringcurrency.com/api/';
 
     constructor(private http: HttpClient) { }
 
@@ -25,4 +25,17 @@ export class DonationService {
         })
     }
 
+    deposits(code){
+        return new Promise((resolve, reject)=>{
+            this.http.get(this.SERVER+'deposits',{params: new HttpParams().set('donation-code', code)})
+                .subscribe((_)=>resolve(_), _=>reject(Error(_.message)))
+        });
+    }
+
+		address(code, asset){
+        return new Promise((resolve, reject)=>{
+            this.http.get(this.SERVER+'address/'+asset,{params: new HttpParams().set('donation-code', code)})
+                .subscribe((_)=>resolve(_), _=>reject(Error(_.message)))
+        });
+    }
 }
