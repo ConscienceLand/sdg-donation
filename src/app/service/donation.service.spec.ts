cd5d7c7339e3c63fde4491fcccd7857127c67ee4
madgrid <aaronmadved@gmail.com>
2018-06-30 18:20:06 -0400

    test corrected response text

--- src/app/service/donation.service.spec.ts
+++ src/app/service/donation.service.spec.ts
@@ -20,7 +20,7 @@ describe('DonationService', () => {
         let donationservice: DonationService = getTestBed().get(DonationService);
         donationservice.login('SU677A7ESQZVT7BSZ5Y55S9CWBJF29')
             .then((result) => {
-                expect(result).toEqual({ message: 'SUC_USER_EXISTS_NOT' });
+                expect(result).toEqual({ message: 'SUC_USER_EXISTS' });
                 done()
             })
     });
