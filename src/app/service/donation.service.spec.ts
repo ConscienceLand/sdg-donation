c7084b45bb08a0b38a2ed413b61f40683eed21e5
madgrid <aaronmadved@gmail.com>
2018-06-30 18:17:11 -0400

    test rename response text

--- src/app/service/donation.service.spec.ts
+++ src/app/service/donation.service.spec.ts
@@ -20,7 +20,7 @@ describe('DonationService', () => {
         let donationservice: DonationService = getTestBed().get(DonationService);
         donationservice.login('SU677A7ESQZVT7BSZ5Y55S9CWBJF29')
             .then((result) => {
-                expect(result).toEqual({ message: 'SUC_USER_EXISTS' });
+                expect(result).toEqual({ message: 'SUC_USER_EXISTS_NOT' });
                 done()
             })
     });
