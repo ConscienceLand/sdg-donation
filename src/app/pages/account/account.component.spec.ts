5c82298953b4bd290b0ef2b7eef3c9b42ec997ef
madgrid <aaronmadved@gmail.com>
2018-06-30 16:12:13 -0400

    Added declared imports to jasmine

--- src/app/pages/account/account.component.spec.ts
+++ src/app/pages/account/account.component.spec.ts
@@ -1,7 +1,11 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing'
+import { RouterTestingModule } from '@angular/router/testing';
 import { AccountComponent } from './account.component';
+import { DonationService } from '../../service/donation.service';
+import { HttpClientModule } from '@angular/common/http';
+import { CookieService } from 'ngx-cookie-service';
 
+import { TranslateModule } from '@ngx-translate/core';
 describe('AccountComponent', () => {
     let component: AccountComponent;
     let fixture: ComponentFixture<AccountComponent>;
@@ -9,7 +13,11 @@ describe('AccountComponent', () => {
     beforeEach(async(() => {
         TestBed.configureTestingModule({
             declarations: [ AccountComponent ],
-            imports:[RouterTestingModule]
+            imports:[RouterTestingModule,
+                     HttpClientModule,
+                     TranslateModule.forRoot(),
+                    ],
+            providers: [ DonationService, CookieService ]
         })
             .compileComponents();
     }));
